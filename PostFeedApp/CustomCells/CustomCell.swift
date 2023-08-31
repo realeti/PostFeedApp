bed4810fabdf718ba689300dd35a5be67a5537fc
bristol <oro.eto.bristol@gmail.com>
2023-08-31 22:07:06 +0300

    Added first scene

---
+++ PostFeedApp/CustomCells/CustomCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomCell.swift
+//  PostFeedApp
+//
+//  Created by Apple M1 on 31.08.2023.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
---
+++ PostFeedApp/Services/PostFeed.swift
@@ -0,0 +1,8 @@
+//
+//  PostFeed.swift
+//  PostFeedApp
+//
+//  Created by Apple M1 on 31.08.2023.
+//
+
+import Foundation
