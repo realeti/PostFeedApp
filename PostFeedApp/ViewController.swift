5baa47d07481bea295663355494161279992525b
bristol <oro.eto.bristol@gmail.com>
2023-08-31 08:41:37 +0300

    Initial Commit

---
+++ PostFeedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostFeedApp
+//
+//  Created by Apple M1 on 31.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
